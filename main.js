a935b9d62ec0adf5adbedd6e680fcc2b27d5c88d
Maricio30 <mauricioescobarmaricarmen30@gmail.com>
2026-01-01 14:53:50 -0500

    Probando otra pagina, cambios

---
+++ main.js
@@ -0,0 +1,9 @@
+const toggle = document.querySelector(".nav__toggle");
+const nav = document.querySelector(".nav");
+
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    const isOpen = nav.classList.toggle("is-open");
+    toggle.setAttribute("aria-expanded", String(isOpen));
+  });
+}
